f5c1102a24ed4b9d923922cc0076ae291af2a937
Atmos4 <atmos4.dev@gmail.com>
2024-09-12 11:13:25 +0200

    refine inline css for lazy-load iframe

--- examples/php_lazy_loading/lazy.php
+++ examples/php_lazy_loading/lazy.php
@@ -2,6 +2,6 @@
     <p>The content below will be lazy loaded!</p>
     <progress id="target">
     </progress>
-    <iframe src="slow.php#target" loading=lazy style="height:0;opacity:0"
+    <iframe src="slow.php#target" loading="lazy" style="height:0;width:0;border:0;opacity:0"
         onload="setTimeout(()=>document.querySelector(contentWindow.location.hash||null)?.replaceWith(...contentDocument.body.childNodes))"></iframe>
 </div>
